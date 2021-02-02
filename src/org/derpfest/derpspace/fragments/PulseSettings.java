e878a98b60026c75dc7847160507e5eeaf52da2f
TikkiTikki <radstylemusic@gmail.com>
2021-02-02 14:59:54 -0300

    DerpSpace: Extend Pulse to QS Panel [2/2]
    Change-Id: Ie9bb0a34dd45a0b8d30c948748ae14a47bbe8cc8
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/org/derpfest/derpspace/fragments/PulseSettings.java
+++ src/org/derpfest/derpspace/fragments/PulseSettings.java
@@ -44,6 +44,7 @@ public class PulseSettings extends SettingsPreferenceFragment implements
 
     private static final String NAVBAR_PULSE_ENABLED_KEY = "navbar_pulse_enabled";
     private static final String LOCKSCREEN_PULSE_ENABLED_KEY = "lockscreen_pulse_enabled";
+    private static final String QS_PULSE_ENABLED_KEY = "qs_pulse_enabled";
     private static final String AMBIENT_PULSE_ENABLED_KEY = "ambient_pulse_enabled";
     private static final String PULSE_SMOOTHING_KEY = "pulse_smoothing_enabled";
     private static final String PULSE_COLOR_MODE_KEY = "pulse_color_mode";
@@ -66,6 +67,7 @@ public class PulseSettings extends SettingsPreferenceFragment implements
 
     private SwitchPreference mNavbarPulse;
     private SwitchPreference mLockscreenPulse;
+    private SwitchPreference mQsPulse;
     private SwitchPreference mAmbientPulse;
     private SwitchPreference mPulseSmoothing;
     private SwitchPreference mPulseCenterMirrored;
@@ -99,6 +101,12 @@ public class PulseSettings extends SettingsPreferenceFragment implements
                 Settings.Secure.LOCKSCREEN_PULSE_ENABLED, 1, UserHandle.USER_CURRENT) != 0;
         mLockscreenPulse.setChecked(lockscreenPulse);
         mLockscreenPulse.setOnPreferenceChangeListener(this);
+
+        mQsPulse = (SwitchPreference) findPreference(QS_PULSE_ENABLED_KEY);
+        boolean qsPulse = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.QS_PULSE_ENABLED, 0, UserHandle.USER_CURRENT) != 0;
+        mQsPulse.setChecked(qsPulse);
+        mQsPulse.setOnPreferenceChangeListener(this);
 
         mAmbientPulse = (SwitchPreference) findPreference(AMBIENT_PULSE_ENABLED_KEY);
         boolean ambientPulse = Settings.Secure.getIntForUser(resolver,
@@ -148,6 +156,12 @@ public class PulseSettings extends SettingsPreferenceFragment implements
                 Settings.Secure.LOCKSCREEN_PULSE_ENABLED, val ? 1 : 0, UserHandle.USER_CURRENT);
             updateAllPrefs();
             return true;
+        } else if (preference == mQsPulse) {
+            boolean val = (Boolean) newValue;
+            Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.QS_PULSE_ENABLED, val ? 1 : 0, UserHandle.USER_CURRENT);
+            updateAllPrefs();
+            return true;
         } else if (preference == mAmbientPulse) {
             boolean val = (Boolean) newValue;
             Settings.Secure.putIntForUser(resolver,
@@ -172,18 +186,21 @@ public class PulseSettings extends SettingsPreferenceFragment implements
         boolean lockscreenPulse = Settings.Secure.getIntForUser(resolver,
                 Settings.Secure.LOCKSCREEN_PULSE_ENABLED, 0, UserHandle.USER_CURRENT) != 0;
 
+        boolean qsPulse = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.QS_PULSE_ENABLED, 1, UserHandle.USER_CURRENT) != 0;
+
         boolean ambientPulse = Settings.Secure.getIntForUser(resolver,
                 Settings.Secure.AMBIENT_PULSE_ENABLED, 0, UserHandle.USER_CURRENT) != 0;
 
-        mPulseSmoothing.setEnabled(navbarPulse || lockscreenPulse || ambientPulse);
+        mPulseSmoothing.setEnabled(navbarPulse || lockscreenPulse || qsPulse || ambientPulse);
 
-        mPulseCenterMirrored.setEnabled(navbarPulse || lockscreenPulse || ambientPulse);
+        mPulseCenterMirrored.setEnabled(navbarPulse || lockscreenPulse || qsPulse || ambientPulse);
 
-        mPulseVerticalMirror.setEnabled(navbarPulse || lockscreenPulse || ambientPulse);
+        mPulseVerticalMirror.setEnabled(navbarPulse || lockscreenPulse || qsPulse || ambientPulse);
 
-        mPulseGravity.setEnabled(navbarPulse || lockscreenPulse || ambientPulse);
+        mPulseGravity.setEnabled(navbarPulse || lockscreenPulse || qsPulse || ambientPulse);
 
-        mColorModePref.setEnabled(navbarPulse || lockscreenPulse || ambientPulse);
+        mColorModePref.setEnabled(navbarPulse || lockscreenPulse || qsPulse || ambientPulse);
         if (navbarPulse || lockscreenPulse) {
             int colorMode = Settings.Secure.getIntForUser(resolver,
                 Settings.Secure.PULSE_COLOR_MODE, COLOR_TYPE_LAVALAMP, UserHandle.USER_CURRENT);
@@ -193,8 +210,8 @@ public class PulseSettings extends SettingsPreferenceFragment implements
             mLavaSpeedPref.setEnabled(false);
         }
 
-        mRenderMode.setEnabled(navbarPulse || lockscreenPulse || ambientPulse);
-        if (navbarPulse || lockscreenPulse || ambientPulse) {
+        mRenderMode.setEnabled(navbarPulse || lockscreenPulse || qsPulse || ambientPulse);
+        if (navbarPulse || lockscreenPulse || qsPulse || ambientPulse) {
             int renderMode = Settings.Secure.getIntForUser(resolver,
                 Settings.Secure.PULSE_RENDER_STYLE, RENDER_STYLE_SOLID_LINES, UserHandle.USER_CURRENT);
             updateRenderCategories(renderMode);
