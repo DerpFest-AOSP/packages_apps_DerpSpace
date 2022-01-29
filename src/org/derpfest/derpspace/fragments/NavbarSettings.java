7347baec77ada7da2ea98d3c891af8e4bf38caba
spezi77 <spezi7713@gmx.net>
2022-01-29 20:14:51 +0100

    NavbarSettings: Convert into DashboardFragment
    * In preparation of adding an AbstractPreferenceController
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: Ic728f3cf29a2a995dd5a1de7d76fd3849241ad2b

--- src/org/derpfest/derpspace/fragments/NavbarSettings.java
+++ src/org/derpfest/derpspace/fragments/NavbarSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 The PixelDust Project
+ * Copyright (C) 2021-2022 The PixelDust Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 
 package org.derpfest.derpspace.fragments;
 
-import android.app.ActivityManagerNative;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -33,26 +32,33 @@ import android.view.View;
 import android.view.IWindowManager;
 import android.view.WindowManagerGlobal;
 
-import androidx.preference.ListPreference;
-import androidx.preference.Preference;
-import androidx.preference.PreferenceCategory;
-import androidx.preference.PreferenceScreen;
+import androidx.fragment.app.Fragment;
+import androidx.preference.*;
 import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.SwitchPreference;
 
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.internal.util.derp.derpUtils;
 import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.development.OverlayCategoryPreferenceController;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+import com.android.settingslib.search.SearchIndexable;
 
 import com.derp.support.preferences.SystemSettingSwitchPreference;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class NavbarSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+@SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
+public class NavbarSettings extends DashboardFragment implements OnPreferenceChangeListener {
+
+    public static final String TAG = "NavbarSettings";
 
     private static final String KEY_NAVIGATION_BAR_ENABLED = "navbar_visibility";
     private static final String LAYOUT_SETTINGS = "layout_settings";
@@ -65,12 +71,11 @@ public class NavbarSettings extends SettingsPreferenceFragment implements OnPref
     private boolean mIsNavSwitchingMode = false;
 
     private Handler mHandler;
+    private Context mContext;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        addPreferencesFromResource(R.xml.navbar_settings);
 
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefSet = getPreferenceScreen();
@@ -98,16 +103,6 @@ public class NavbarSettings extends SettingsPreferenceFragment implements OnPref
     }
 
     @Override
-    public int getMetricsCategory() {
-        return MetricsEvent.DERP;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         if (preference == mNavigationBar) {
             boolean value = (Boolean) objValue;
@@ -127,4 +122,36 @@ public class NavbarSettings extends SettingsPreferenceFragment implements OnPref
         }
         return false;
     }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.DERP;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.navbar_settings;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, getSettingsLifecycle(), this);
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(
+            Context context, Lifecycle lifecycle, Fragment fragment) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        return controllers;
+    }
+
+    /**
+     * For Search.
+     */
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.navbar_settings);
 }
