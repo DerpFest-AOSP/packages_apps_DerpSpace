0d1fb8a5760b788dca186eb2e9be078222ba6e5b
Ido Ben-Hur <idoybh2@gmail.com>
2022-12-03 15:28:40 +0200

    DerpSpace: Add native monet settings to change theme style
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/org/derpfest/derpspace/fragments/MonetEngine.java
+++ src/org/derpfest/derpspace/fragments/MonetEngine.java
@@ -22,21 +22,20 @@ import android.os.Bundle;
 import android.content.DialogInterface;
 import android.view.ViewGroup;
 import android.os.UserHandle;
-import android.os.UserManager;
 import android.provider.Settings;
 
 import android.content.res.Resources;
 import android.app.AlertDialog;
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
-import androidx.preference.SwitchPreference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.Preference.OnPreferenceClickListener;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-import java.util.ArrayList;
-import java.util.List;
 import com.skydoves.colorpickerview.ActionMode;
 import com.skydoves.colorpickerview.ColorEnvelope;
 import com.skydoves.colorpickerview.ColorPickerDialog;
@@ -45,14 +44,19 @@ import com.skydoves.colorpickerview.listeners.ColorListener;
 import com.skydoves.colorpickerview.listeners.ColorPickerViewListener;
 import com.skydoves.colorpickerview.preference.ColorPickerPreferenceManager;
 import org.derpfest.derpspace.utils.WallpaperUtils;
-import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.Preference.OnPreferenceClickListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MonetEngine extends SettingsPreferenceFragment implements
-        OnPreferenceClickListener {
+        OnPreferenceChangeListener, OnPreferenceClickListener {
 
     final static String TAG = "MonetEngine";
 
+    private static final String OVERLAY_CATEGORY_THEME_STYLE =
+            "android.theme.customization.theme_style";
+
+    private static final String PREF_THEME_STYLE = "theme_style";
     private static final String PREF_CHROMA_FACTOR ="monet_engine_chroma_factor";
     private static final String PREF_LUMINANCE_FACTOR ="monet_engine_luminance_factor";
     private static final String PREF_TINT_BACKGROUND ="monet_engine_tint_background";
@@ -62,6 +66,7 @@ public class MonetEngine extends SettingsPreferenceFragment implements
     private static final String PREF_BGCOLOR_OVERRIDE ="monet_engine_bgcolor_override";
     private static final String PREF_WALLPAPER_COLOR ="monet_engine_wall_color_select";
 
+    private ListPreference mThemeStylePref;
     private Preference mWallColorPicker;
     public ColorPickerView mColorPickerView;
     public int mUserColor;
@@ -89,6 +94,88 @@ public class MonetEngine extends SettingsPreferenceFragment implements
         mWallColorPicker.setOnPreferenceClickListener(this);
         mWallColorPicker.setEnabled(!WallpaperUtils.isLiveWall(getContext()));
         mWallColorPicker.setSummary(WallpaperUtils.isLiveWall(getContext()) ? R.string.not_available : R.string.monet_engine_wallpaper_color_select_summary);
+
+        mThemeStylePref = findPreference(PREF_THEME_STYLE);
+        updatePreferences();
+        mThemeStylePref.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mThemeStylePref) {
+            String value = (String) newValue;
+            setStyleValue(value);
+            updateListByValue(mThemeStylePref, value, false);
+            return true;
+        }
+        return false;
+    }
+
+    private void updatePreferences() {
+        final String overlayPackageJson = Settings.Secure.getStringForUser(
+                getActivity().getContentResolver(),
+                Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES,
+                UserHandle.USER_CURRENT);
+        if (overlayPackageJson != null && !overlayPackageJson.isEmpty()) {
+            try {
+                final JSONObject object = new JSONObject(overlayPackageJson);
+                final String style = object.optString(OVERLAY_CATEGORY_THEME_STYLE, null);
+                // style handling
+                boolean styleUpdated = false;
+                if (style != null && !style.isEmpty()) {
+                    for (CharSequence value : mThemeStylePref.getEntryValues()) {
+                        if (value.toString().equals(style)) {
+                            styleUpdated = true;
+                            break;
+                        }
+                    }
+                    if (styleUpdated) {
+                        updateListByValue(mThemeStylePref, style);
+                    }
+                }
+                if (!styleUpdated) {
+                    updateListByValue(mThemeStylePref,
+                            mThemeStylePref.getEntryValues()[0].toString());
+                }
+            } catch (JSONException | IllegalArgumentException ignored) {}
+        }
+    }
+
+    private void updateListByValue(ListPreference pref, String value) {
+        updateListByValue(pref, value, true);
+    }
+
+    private void updateListByValue(ListPreference pref, String value, boolean set) {
+        if (set) pref.setValue(value);
+        final int index = pref.findIndexOfValue(value);
+        pref.setSummary(pref.getEntries()[index]);
+    }
+
+    private JSONObject getSettingsJson() throws JSONException {
+        final String overlayPackageJson = Settings.Secure.getStringForUser(
+                getActivity().getContentResolver(),
+                Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES,
+                UserHandle.USER_CURRENT);
+        JSONObject object;
+        if (overlayPackageJson == null || overlayPackageJson.isEmpty())
+            return new JSONObject();
+        return new JSONObject(overlayPackageJson);
+    }
+
+    private void putSettingsJson(JSONObject object) {
+        Settings.Secure.putStringForUser(
+                getActivity().getContentResolver(),
+                Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES,
+                object.toString(), UserHandle.USER_CURRENT);
+    }
+
+    private void setStyleValue(String style) {
+        try {
+            JSONObject object = getSettingsJson();
+            object.putOpt(OVERLAY_CATEGORY_THEME_STYLE, style);
+            putSettingsJson(object);
+        } catch (JSONException | IllegalArgumentException ignored) {}
     }
 
     @Override
