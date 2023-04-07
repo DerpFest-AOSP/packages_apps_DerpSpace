3cf769d3e56c8a6853a91c6d7a46943224aa5880
Ido Ben-Hur <idoybh2@gmail.com>
2023-04-07 03:06:40 +0200

    DerpSpace: LockscreenShortcuts: Avoid joining strings with + in a loop
    Properly use the StringBuilder there

--- src/org/derpfest/derpspace/fragments/LockscreenShortcuts.java
+++ src/org/derpfest/derpspace/fragments/LockscreenShortcuts.java
@@ -97,11 +97,11 @@ public class LockscreenShortcuts extends SettingsPreferenceFragment implements O
         if (value == null || value.isEmpty()) {
             StringBuilder sb = new StringBuilder(DEFAULT_START_SHORTCUT[0]);
             for (int i = 1; i < DEFAULT_START_SHORTCUT.length; i++) {
-                sb.append("," + DEFAULT_START_SHORTCUT[i]);
+                sb.append(",").append(DEFAULT_START_SHORTCUT[i]);
             }
             sb.append(";" + DEFAULT_END_SHORTCUT[0]);
             for (int i = 1; i < DEFAULT_END_SHORTCUT.length; i++) {
-                sb.append("," + DEFAULT_END_SHORTCUT[i]);
+                sb.append(",").append(DEFAULT_END_SHORTCUT[i]);
             }
             value = sb.toString();
         }
@@ -135,7 +135,7 @@ public class LockscreenShortcuts extends SettingsPreferenceFragment implements O
             final String[] def = start ? DEFAULT_START_SHORTCUT : DEFAULT_END_SHORTCUT;
             for (String str : def) {
                 if (str.equals(value)) continue;
-                sb.append("," + str);
+                sb.append(",").append(str);
             }
             split[splitIndex] = sb.toString();
         }
