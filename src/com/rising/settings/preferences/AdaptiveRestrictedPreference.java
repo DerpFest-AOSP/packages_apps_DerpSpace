6cc4b9592d22f0e02aa7536fd4679d2bdd530729
minaripenguin <minaripenguin@users.noreply.github.com>
2023-06-06 12:28:04 +0800

    settings: Introduce RisingUI [1/n]
    * import adaptive preference for rising-sdk migration after UpsideDownCake release
    
    Change-Id: I51238bcfd07f0353ebcc0d7a236b2e38f5d04e81
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

---
+++ src/com/rising/settings/preferences/AdaptiveRestrictedPreference.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2023 The risingOS Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.rising.settings.preferences;
+
+import android.content.Context;
+import android.os.Process;
+import android.os.UserHandle;
+import android.util.AttributeSet;
+import android.content.res.TypedArray;
+import androidx.core.content.res.TypedArrayUtils;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+import android.view.View;
+
+import com.android.settings.R;
+
+import com.android.settingslib.RestrictedPreference;
+
+public class AdaptiveRestrictedPreference extends RestrictedPreference {
+    
+    private Position position;
+
+    public AdaptiveRestrictedPreference(Context context, AttributeSet attrs,
+                                        int defStyleAttr, int defStyleRes, String packageName, int uid) {
+        super(context, attrs, defStyleAttr, defStyleRes, packageName, uid);
+        init(context, attrs);
+    }
+
+    public AdaptiveRestrictedPreference(Context context, AttributeSet attrs,
+                                        int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
+    }
+
+    public AdaptiveRestrictedPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    public AdaptiveRestrictedPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public AdaptiveRestrictedPreference(Context context) {
+        super(context);
+    }
+
+    public AdaptiveRestrictedPreference(Context context, String packageName, int uid) {
+        super(context, packageName, uid);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        // Retrieve and set the layout resource based on position
+        // otherwise do not set any layout
+        position = getPosition(context, attrs);
+        if (position != null) {
+            int layoutResId = getLayoutResourceId(position);
+            setLayoutResource(layoutResId);
+        }
+    }
+
+
+    private Position getPosition(Context context, AttributeSet attrs) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.AdaptivePreference);
+        String positionAttribute = typedArray.getString(R.styleable.AdaptivePreference_position);
+        typedArray.recycle();
+
+        Position positionFromAttribute = Position.fromAttribute(positionAttribute);
+        if (positionFromAttribute != null) {
+            return positionFromAttribute;
+        }
+
+        return null;
+    }
+
+    private int getLayoutResourceId(Position position) {
+        switch (position) {
+            case TOP:
+                return R.layout.arc_card_about_top;
+            case BOTTOM:
+                return R.layout.arc_card_about_bottom;
+            case MIDDLE:
+                return R.layout.arc_card_about_middle;
+            default:
+                return R.layout.arc_card_about_middle;
+        }
+    }
+
+    private enum Position {
+        TOP,
+        MIDDLE,
+        BOTTOM;
+
+        public static Position fromAttribute(String attribute) {
+            if (attribute != null) {
+                switch (attribute.toLowerCase()) {
+                    case "top":
+                        return TOP;
+                    case "bottom":
+                        return BOTTOM;
+                    case "middle":
+                        return MIDDLE;
+                        
+                }
+            }
+            return null;
+        }
+    }
+}
+
