d9809b9bd83c62f6be0e812a2aebf3d54d32ba30
pimpmaneaton <shaunsibu@gmail.com>
2024-02-11 01:38:52 +0100

    DerpSpace: Ambient batterybar color options [2/2]

--- src/org/derpfest/derpspace/fragments/AmbientUI.java
+++ src/org/derpfest/derpspace/fragments/AmbientUI.java
@@ -48,14 +48,37 @@ import com.android.settingslib.search.SearchIndexable;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.Utils;
 
+import org.derpfest.support.preferences.SystemSettingListPreference;
+import org.derpfest.support.colorpicker.ColorPickerPreference;
+
 @SearchIndexable
 public class AmbientUI extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String OMNI_KEYGUARD_BATTERY_BAR_COLOR_SOURCE = "sysui_keyguard_battery_bar_color_source";
+    private static final String OMNI_KEYGUARD_BATTERY_BAR_CUSTOM_COLOR = "sysui_keyguard_battery_bar_custom_color";
+
+    private SystemSettingListPreference mBarColorSource;
+    private ColorPickerPreference mBarCustomColor;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.ambient_ui);
+
+        // ambient batterybar color type
+        mBarColorSource = (SystemSettingListPreference) findPreference(OMNI_KEYGUARD_BATTERY_BAR_COLOR_SOURCE);
+        mBarColorSource.setValue(String.valueOf(Settings.System.getInt(
+                getContentResolver(), Settings.System.OMNI_KEYGUARD_BATTERY_BAR_COLOR_SOURCE, 0)));
+        mBarColorSource.setSummary(mBarColorSource.getEntry());
+        mBarColorSource.setOnPreferenceChangeListener(this);
+
+        mBarCustomColor = (ColorPickerPreference) findPreference(OMNI_KEYGUARD_BATTERY_BAR_CUSTOM_COLOR);
+        mBarCustomColor.setOnPreferenceChangeListener(this);
+        int batteryBarColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.OMNI_KEYGUARD_BATTERY_BAR_CUSTOM_COLOR, 0xFF39FF42);
+        String batteryBarColorHex = String.format("#%08x", (0xFF39FF42 & batteryBarColor));
+        mBarCustomColor.setNewPreviewColor(batteryBarColor);
     }
 
     @Override
@@ -65,6 +88,27 @@ public class AmbientUI extends SettingsPreferenceFragment implements OnPreferenc
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+         if (preference == mBarColorSource) {
+             int value = Integer.valueOf((String) objValue);
+             int vIndex = mBarColorSource.findIndexOfValue((String) objValue);
+             mBarColorSource.setSummary(mBarColorSource.getEntries()[vIndex]);
+             Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.OMNI_KEYGUARD_BATTERY_BAR_COLOR_SOURCE, value);
+            if (value == 2) {
+                mBarCustomColor.setEnabled(true);
+            } else {
+                mBarCustomColor.setEnabled(false);
+            }
+            return true;
+        } else if (preference == mBarCustomColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(objValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.OMNI_KEYGUARD_BATTERY_BAR_CUSTOM_COLOR, intHex);
+            return true;
+        }
         return false;
     }
 
